03dbc16c67f7b9524d0857d63f21dc7bfe7b4bff
developer1 <rafi.huta@gmail.com>
2023-08-26 15:56:05 +0530

    Added groovy file

---
+++ var/gitCheckout.groovy
@@ -0,0 +1,11 @@
+Library('my-shared-lib') _
+
+def call(Map stageParams) {
+
+	checkout ([
+	    $class: 'GitSCM'
+	    branches: [[name: stageParams.branch ]],
+	    userRemoteConfigs: [[ url: stageParams.url ]]
+
+	])
+}
