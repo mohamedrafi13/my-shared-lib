3d8467eccf1b12fe3c519a14f6037d7da725b338
developer1 <rafi.huta@gmail.com>
2023-08-28 12:45:52 +0530

    Added the correction

--- vars/qualityGateCheck.groovy
+++ vars/qualityGateCheck.groovy
@@ -1,5 +1,5 @@
 
 def call(credentialsId){
-    waitForQualityGate abortPipeline: false, credentialsId: credentialsId
+   waitForQualityGate abortPipeline: false, credentialsId: credentialsId
 }
     
