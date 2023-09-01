ab8c59ed9194aa4baf1837465999e3df766b0fc2
developer1 <rafi.huta@gmail.com>
2023-09-01 23:52:58 +0530

    String spell or Class  changed

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
-def call(string project, string ImageTag, string dockerHub) {
+def call(String project, String ImageTag, String dockerHub) {
     sh """
-    docker image build -t ${docker}/${project} .
+    docker image build -t ${dockerhub}/${project} .
     docker image tag ${dockerhub}/${project} ${dockerhub}/${project}:${ImageTag}
     docker image tag ${dockerhub}/${project} ${dockerhub}/${project}:latest
     
