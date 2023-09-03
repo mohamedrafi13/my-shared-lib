1f64e4590696ff8cdd0d2a797edcde01567ed1b7
developer1 <rafi.huta@gmail.com>
2023-09-03 23:18:13 +0530

    changed the variable

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTag, String dockerHub) {
     sh """
-    trivy image ${dockerhub}/${project}:latest > scan.txt
+    trivy image ${dockerHub}/${project}:latest > scan.txt
     cat scan.txt
     
     """
