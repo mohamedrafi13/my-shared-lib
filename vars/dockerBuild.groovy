1efd9d1d471501ac7079b69a416a1cc74b1c631e
developer1 <rafi.huta@gmail.com>
2023-09-05 23:39:49 +0530

    typo corrected

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,18 @@
-def call(String project, String ImageTag, String dockerHub) {
+/*def call(String project, String ImageTag, String dockerHub) {
     sh """
     docker image build -t ${dockerHub}/${project} .
     docker image tag ${dockerHub}/${project} ${dockerHub}/${project}:${ImageTag}
     docker image tag ${dockerHub}/${project} ${dockerHub}/${project}:latest
     
+    """
+    
+}*/
+
+
+def call(String awsAccountID, String region, String ecrRepoName) {
+    sh """
+    docker build -t ${ecrRepoName} .
+    docker tag ${ecrRepoName}:latest ${awsAccountID}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest
     """
     
 }
