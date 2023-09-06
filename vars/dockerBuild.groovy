d63beef2b1f76ea0f62abccaf891fb1250769ef4
developer1 <rafi.huta@gmail.com>
2023-09-06 15:49:32 +0530

    changed the code

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -12,7 +12,7 @@
 def call(String awsAccountID, String region, String ecrRepoName) {
     sh """
     docker build -t ${ecrRepoName} .
-    docker tag ${awsAccountID}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest
+    docker tag ${ecr_repoName}:latest ${awsAccountID}.dkr.ecr.${region}.amazonaws.com/${ecrRepoName}:latest
     """
     
 }
